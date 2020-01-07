f33e0996663f413675bc840a5c72ad1f917ab893
theMightyT <tvanrys@fanshaweonline.ca>
2020-01-07 12:14:41 -0500

    start point

--- admin/functions.php
+++ admin/functions.php
@@ -1,17 +1,1 @@
 <?php
-
-    require('connect.php');
-
-    function getPlanetData($conn) {
-        $getData = 'SELECT * FROM info';
-        $runQuery = $conn->query($getData);
-
-        $result = array();
-
-        while($row = $runQuery->fetch(PDO::FETCH_ASSOC)) {
-            // push each row of data into our array
-            $result[] = $row;
-        }
-
-        return $result;
-    }
--- index.php
+++ index.php
@@ -1,28 +1,3 @@
-<?php
-    require_once('./admin/functions.php');
-
-    $planetCollection = getPlanetData($pdo);
-
-    var_dump($planetCollection);
-
-    $dummy_result = [
-        "title" => "this is a title",
-        "description" => "this is a description",
-        "text" => "this is some text",
-        "somevalues" => ["one,two,three,four"],
-        "otherstuff" => "whatever"
-    ];
-
-    $my_csv = ["This,is,some,text"];
-
-    $test = $dummy_result["somevalues"];
-
-    $exploded = explode(",", $dummy_result["somevalues"][0]);
-
-    var_dump($exploded);
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -40,28 +15,9 @@
         <h2>The first three planets, closest to Sol:</h2>
 
         <!-- this is an inline php block -->
-        <?php foreach ($planetCollection as $planet) {
-            echo '<div class="planet">' .
-                    '<img class="planet-img" src="' . $planet["IMGPATH"] . '" alt="planet image">' .
-                    '<h4 class="planet-name">' . $planet["NAME"] . ': ' . $planet["ID"] . '</h4>' .
-                    '<p class="planet-description">' . $planet["DESCRIPTION"] . '</p>' .
-                '</div>';
-        } ?>
-
-        </div>
+        
 
         <!-- end php block -->
-
-        <!-- write out our php stuff here -->
-        <!-- <h1 class="hidden">Test Explode!</h1>
-        <p class="exploded-values">
-            <span class="subhead">Exploded text follows:</span>
-            <ul class="text-from-php">
-            <?php foreach ($exploded as $text) {
-                echo '<li class="text-item">' . $text . '</li>';
-            } ?>
-            </ul>
-        </p> -->
     </main>
 </body>
 </html>
